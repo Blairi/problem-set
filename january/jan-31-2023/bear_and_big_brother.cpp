5027fa5266879dbe266edc17437ea155ed6c8baa
Blairi <axel.avi.fer@gmail.com>
2023-01-31 16:27:07 -0600

    #1 jan-31-2023

---
+++ january/jan-31-2023/bear_and_big_brother.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/contest/791/submission/191443648
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b; cin >> a >> b;
+    int y = 0;
+    while(a <= b)
+    {
+	a *= 3;
+	b *= 2;
+	y ++;
+    }
+    cout << y << endl;
+}
+
