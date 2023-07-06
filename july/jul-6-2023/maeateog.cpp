aa80736f525a8ed5a3b175e3391c2efdf84fa0b5
Blairi <axel.avi.fer@gmail.com>
2023-07-06 14:49:45 -0600

    #2 jul-6-2023

---
+++ july/jul-6-2023/maeateog.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int n; cin >> n;
+    if(n % 2 == 0)
+        cout << "Mahmoud" << endl;
+    else
+        cout << "Ehab" << endl;
+}
+
+int main()
+{
+    int t = 1;
+    /* cin >> t; */
+    
+    while (t--) 
+    {
+        solve();
+    }
+
+    return 0;
+}
