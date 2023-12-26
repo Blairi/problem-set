96b2ab5a8f95757e737fe332edbe0c73da125c0c
Blairi <axel.avi.fer@gmail.com>
2023-12-25 19:27:09 -0600

    #1 dec-25-2023

---
+++ december-23/domino_piling.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/contest/50/submission/238896584
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+    cout << n*m/2 << endl;
+}
+
+int main()
+{
+    int t = 1;
+    /* cin >> t; */
+    
+    while (t--) 
+    {
+        solve();
+    }
+
+    return 0;
+}
